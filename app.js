59b450bdb8b51c1f916eb7e9d53ead0dfb980cdb
preksha <jainpreksha2712@gmail.com>
2023-02-13 23:12:41 +0530

    change allow access

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 dotenv.config();
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000, https://compos.netlify.app/');
     res.setHeader('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE')
